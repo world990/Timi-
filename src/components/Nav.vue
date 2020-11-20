752f038ee21a78b4db547d02c9777000a18dde93
world990 <342721572@qq.com>
2020-11-20 16:34:17 +0800

    页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="nav">
     <router-link to="/money">记账</router-link>
     |
     <router-link to="/labels">标签</router-link>
@@ -10,10 +10,15 @@
 
 <script>
 export default {
-name: "Nav"
+  name: 'Nav'
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.nav {
+  border: 1px solid red;
+
+
+}
 
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,16 +1,28 @@
 <template>
-<div>
-  money.vue
-  <Nav/>
-</div>
+  <div class="nav-wrapper">
+    <div class="content">
+      money.vue
+    </div>
+    <Nav/>
+  </div>
 </template>
 
 <script>
 export default {
-name: "money"
+  name: 'money'
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
