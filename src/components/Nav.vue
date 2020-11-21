d13cb6b296bca654063601703fa25e7feabe83c6
world990 <342721572@qq.com>
2020-11-21 15:43:00 +0800

    加上了一个loader解决了SVG自带颜色不能改色的问题

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    <router-link to="/money" class="item">
-      <Icon name="money"/>
+    <router-link to="/money" class="item" active-class="selected">
+      <Icon name="money" />
       记账
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -38,12 +38,18 @@ nav {
     padding: 2px 0;
 
     .icon {
-      height: 30px;
-      width: 30px;
+      height: 32px;
+      width: 32px;
     }
+  }
+
+  > .item.selected {
+    color: red;
   }
 }
 
-
 </style>
+
+
+
 
