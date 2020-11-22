5a09f903d30af52849b6036be40fec96a9e6db27
world990 <342721572@qq.com>
2020-11-22 21:34:45 +0800

    :value="value" @input="value=$event.target.value"可以简写为v-model 完成notes组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -2,14 +2,21 @@
   <div>
     <label class="notes">
       <span class="name">备注</span>
-      <input type="text" placeholder="在这里输入">
+      <input type="text"
+             v-model="value"
+             placeholder="在这里输入">
+
     </label>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'Notes'
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Notes extends Vue {
+  value = '';
 }
 </script>
 
@@ -21,17 +28,17 @@ export default {
   align-items: center;
 
 
-> .name {
-  margin-left: 16px;
-}
+  > .name {
+    margin-left: 16px;
+  }
 
 
-input {
-  height: 64px;
-  flex-grow: 1;
-  border: none;
-  background: transparent;
-  margin-left: 16px;
-}
+  input {
+    height: 64px;
+    flex-grow: 1;
+    border: none;
+    background: transparent;
+    margin-left: 16px;
+  }
 }
 </style>
