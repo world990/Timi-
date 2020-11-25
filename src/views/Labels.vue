e951cb4317559d76a8d876c4b1caffcc5631159c
world990 <342721572@qq.com>
2020-11-25 19:34:36 +0800

    点击标签可以跳转到编辑页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <Layout>
-      <ol class="tags">
-        <li v-for="tag in tags" :key="tag.id"><span>{{ tag.name }}</span>
+      <div class="tags">
+        <router-link
+          class="tag"  v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}`"><span>{{ tag.name }}</span>
           <icon name="right"/>
-        </li>
-      </ol>
+        </router-link>
+      </div>
       <div class="newTag-wrapper">
         <button class="newTag" @click="createTag">新建标签</button>
       </div>
@@ -43,7 +44,7 @@ export default class Labels extends Vue {
   font-size: 16px;
   padding-left: 16px;
 
-  > li {
+  > .tag {
     min-height: 44px;
     display: flex;
     align-items: center;
