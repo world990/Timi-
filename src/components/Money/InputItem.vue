bf19c7578ff43e85a2ce6bf0f43c498e490edf7f
world990 <342721572@qq.com>
2020-11-26 18:07:59 +0800

    value从data变成了prop

--- src/components/Money/InputItem.vue
+++ src/components/Money/InputItem.vue
@@ -17,7 +17,7 @@ import {Component, Prop, Watch} from 'vue-property-decorator';
 
 @Component
 export default class InputItem extends Vue {
-  value = '';
+  @Prop({default: ''}) value!: string;
 
   @Prop({required: true}) fieldName!: string;
   @Prop() placeholder?: string;
