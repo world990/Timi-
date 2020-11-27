4fb1b90317b2eed2b8a093ae0d5026c0a516bb99
world990 <342721572@qq.com>
2020-11-27 16:20:21 +0800

    将 create 封装到 model

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -51,14 +51,12 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const record2: RecordItem = recordListModel.clone(this.record);
-    record2.createAt = new Date();
-    this.recordList.push(record2);
+    recordListModel.create(this.record);
   }
 
   @Watch('recordList')
   onRecordListChange() {
-    recordListModel.save(this.recordList);
+    recordListModel.save();
   }
 }
 </script>
